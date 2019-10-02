94e821ac51ccbdd83b1acacb4368ad6e7aaaf475
Zhenmao Wan <wanzhenmao@gmail.com>
2019-10-02 15:14:20 +0800

    Update announcement list

---
+++ frontend/components/TheAnnouncementList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <TheAnnouncementListItem
+      v-for="(announcement, i) in announcements"
+      :key="i"
+      :item="announcement"
+    />
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import TheAnnouncementListItem from "@/components/TheAnnouncementListItem.vue";
+
+export default {
+  components: {
+    TheAnnouncementListItem
+  },
+  computed: {
+    ...mapState(["announcements"])
+  }
+};
+</script>
+
+<style></style>
---
+++ frontend/components/TheAnnouncementListItem.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="mb-4">
+    <VBorderedCard
+      :to="
+        localePath({
+          name: 'article-id',
+          params: { id: item.id }
+        })
+      "
+    >
+      <div class="announcement-item">
+        <div class="announcement-item-icon">
+          <v-icon x-large color="primary">{{ icon.icon }}</v-icon>
+        </div>
+        <div class="announcement-item-main mx-4">
+          <div class="announcement-item-title accent--text headline mb-2">
+            {{ item.title }}
+          </div>
+          <div class="announcement-item-date black--text">
+            {{ $tc("day", fromNow(item.date)) }}
+          </div>
+        </div>
+        <div class="announcement-item-audio">
+          <v-btn
+            class="mx-2"
+            fab
+            dark
+            color="primary"
+            @click.prevent="playAudio"
+          >
+            <v-icon dark>{{ icon.audio }}</v-icon>
+          </v-btn>
+        </div>
+      </div>
+    </VBorderedCard>
+  </div>
+</template>
+
+<script>
+import { mdiCupWater, mdiVolumeHigh } from "@mdi/js";
+import dayjs from "dayjs";
+import VBorderedCard from "@/components/VBorderedCard.vue";
+export default {
+  components: {
+    VBorderedCard
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      icon: {
+        icon: mdiCupWater,
+        audio: mdiVolumeHigh
+      }
+    };
+  },
+  methods: {
+    playAudio(e) {
+      console.log("Play audio");
+    },
+    fromNow(date) {
+      return dayjs().diff(dayjs(date), "day");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.announcement-item {
+  display: flex;
+}
+
+.announcement-item-icon {
+  width: 64px;
+}
+
+.announcement-item-icon svg {
+  width: 64px !important;
+  height: 64px !important;
+}
+
+.announcement-item-main {
+  flex: 1;
+}
+
+.announcement-item-audio {
+  width: 64px;
+}
+</style>
