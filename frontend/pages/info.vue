d90b37b6d91381fb95953a84077a440bc89df4ed
Zhenmao Wan <wanzhenmao@gmail.com>
2019-11-19 12:54:16 +0800

    Remove information sectors

--- frontend/pages/info.vue
+++ frontend/pages/info.vue
@@ -1,35 +1,35 @@
 <template>
-  <div>
-    <VTitle>
-      <template v-slot:title>
-        {{ $t("information") }}
-      </template>
-      <template v-slot:title-append>
-        <PlayButtonInstruction :src="audioSrc" />
-      </template>
-    </VTitle>
-    <InformationSectorList />
-  </div>
+	<div>
+		<VTitle>
+			<template v-slot:title>
+				{{ $t("information") }}
+			</template>
+			<template v-slot:title-append>
+				<PlayButtonInstruction :src="audioSrc" />
+			</template>
+		</VTitle>
+		<InformationArticleList />
+	</div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import VTitle from "@/components/VTitle";
 import PlayButtonInstruction from "@/components/PlayButtonInstruction";
-import InformationSectorList from "@/components/InformationSectorList.vue";
+import InformationArticleList from "@/components/InformationArticleList.vue";
 export default {
-  components: { VTitle, PlayButtonInstruction, InformationSectorList },
-  computed: {
-    ...mapGetters(["hasAudioInstruction"]),
-    locale() {
-      return this.$i18n.locale;
-    },
-    audioSrc() {
-      const key = "info";
-      return this.hasAudioInstruction(key, this.locale)
-        ? `/audio/${key}/${this.locale}.mp3`
-        : null;
-    }
-  }
+	components: { VTitle, PlayButtonInstruction, InformationArticleList },
+	computed: {
+		...mapGetters(["hasAudioInstruction"]),
+		locale() {
+			return this.$i18n.locale;
+		},
+		audioSrc() {
+			const key = "info";
+			return this.hasAudioInstruction(key, this.locale)
+				? `/audio/${key}/${this.locale}.mp3`
+				: null;
+		}
+	}
 };
 </script>
