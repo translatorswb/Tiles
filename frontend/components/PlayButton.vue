5300a66584cc331adf84242081a80e828235471a
Zhenmao Wan <wanzhenmao@gmail.com>
2019-11-21 22:55:55 +0800

    Change play button icon

--- frontend/components/PlayButton.vue
+++ frontend/components/PlayButton.vue
@@ -14,7 +14,7 @@
 
 <script>
 import { mapState } from "vuex";
-import { mdiVolumeHigh, mdiPause } from "@mdi/js";
+import { mdiVoice, mdiPause } from "@mdi/js";
 export default {
   props: {
     src: {
@@ -33,7 +33,7 @@ export default {
   data() {
     return {
       icon: {
-        play: mdiVolumeHigh,
+        play: mdiVoice,
         pause: mdiPause
       },
       isPlaying: false
